44ddeede3303313da79612b0dbe53a5959cb82aa
BHARATH <119052611+bharathgs001@users.noreply.github.com>
2023-01-06 14:59:49 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class test {
     public static void main(String[] args) {
     	Scanner pk = new Scanner(System.in);
+    	
+    	String a = pk.next();t
     	
     }
 }
